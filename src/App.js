317da5ce82082f3d0027ba65e54c44560a2ff111
abhi-0086 <babaabhishek86@gmail.com>
2023-07-11 11:13:27 +0530

    completed mobile UI and link functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import { useState } from "react";
 import "./App.css";
 import BackgroundAnimate from "./BackgroundAnimate";
 import InputShortener from "./InputShortener";
 import LinkResult from "./LinkResult";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+
   return (
     <div className="container">
-      <InputShortener />
+      <InputShortener setInputValue={setInputValue} />
       <BackgroundAnimate />
-      <LinkResult />
+      <LinkResult inputValue={inputValue} />
     </div>
   );
 }
--- src/InputShortener.js
+++ src/InputShortener.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
-const InputShortener = () => {
+const InputShortener = ({ setInputValue }) => {
+  const [value, setValue] = useState("");
+
+  const handleClick = () => {
+    setInputValue(value);
+    setValue("");
+  };
+
   return (
     <div className="input-container">
       <h1>
         URL <span>Shortener</span>
       </h1>
       <div>
-        <input type="text" placeholder="Paste a link to shorten" />
-        <button>Shorten</button>
+        <input
+          type="text"
+          placeholder="Paste a link to shorten"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button onClick={handleClick}>Shorten</button>
       </div>
     </div>
   );
